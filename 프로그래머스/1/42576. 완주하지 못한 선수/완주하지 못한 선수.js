692779f5c6735b9b1000e2aeb667677094ceee08
hizdev <70252430+gpfksdlrn@users.noreply.github.com>
2025-09-09 22:55:21 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 51.21 ms, Memory: 65.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -2,10 +2,19 @@
 // 단 한 명의 선수를 제외한 완주한 선수들의 이름이 담긴 배열 = completion
 // return -> 완주하지 못한 선수의 이름
 function solution(participant, completion) {
-  participant.sort();
-  completion.sort();
-
-  for (let i in participant) {
-    if(participant[i] !== completion[i]) return participant[i];
-  }
+    const map = new Map();
+    
+    // 참가자들 카운팅
+    participant.forEach(name => {
+        map.set(name, (map.get(name) || 0) + 1);
+    });
+    
+    // 완주자들 차감
+    completion.forEach(name => {
+        map.set(name, map.get(name) - 1);
+    });
+    
+    for (let [name, count] of map) {
+        if(count === 1) return name;
+    }
 }
