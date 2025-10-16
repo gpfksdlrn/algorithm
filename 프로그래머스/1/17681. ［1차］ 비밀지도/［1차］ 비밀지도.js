f04c493417c71ffba682a8475abd9dd6115f18f4
hizdev <70252430+gpfksdlrn@users.noreply.github.com>
2025-10-16 18:17:35 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,9 @@
+function solution(n, arr1, arr2) {
+    return arr1.map((num, i)=>
+        (num | arr2[i])
+        .toString(2)
+        .padStart(n, '0')
+        .replace(/1/g, '#')
+        .replace(/0/g, ' ')
+   );
+}
