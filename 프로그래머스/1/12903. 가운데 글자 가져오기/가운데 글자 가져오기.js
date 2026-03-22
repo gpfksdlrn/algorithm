b6845682c623b7f899ab96f188e5f225f3ac9c0d
hizdev <70252430+gpfksdlrn@users.noreply.github.com>
2026-03-22 21:11:03 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,6 +1,4 @@
 function solution(s) {
-    const middle = Math.floor(s.length / 2);
-    return s.length % 2 === 0 
-        ? s[middle - 1] + s[middle]
-        : s[middle]
+  const middle = Math.floor(s.length / 2);
+  return s.length % 2 === 0 ? s[middle - 1] + s[middle] : s[middle];
 }
