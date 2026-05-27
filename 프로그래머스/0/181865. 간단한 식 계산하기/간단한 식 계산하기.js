3f25cf8b7110c6b4ad0e0ee034a42b7e1a01a502
hizdev <70252430+gpfksdlrn@users.noreply.github.com>
2026-05-27 17:57:30 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.07 ms, Memory: 32.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -0,0 +1,12 @@
+function solution(binomial) {
+    const [a, op, b] = binomial.split(' ');
+
+    const numA = Number(a);
+    const numB = Number(b);
+    
+    switch(op) {
+        case '+': return numA + numB;
+        case '-': return numA - numB;
+        case '*': return numA * numB;
+    }
+}
