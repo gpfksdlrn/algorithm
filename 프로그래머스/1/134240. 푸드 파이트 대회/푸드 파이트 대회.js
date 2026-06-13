1f1469350d7d27ed90c1937d3163b8a84ef85c86
hizdev <70252430+gpfksdlrn@users.noreply.github.com>
2026-06-13 18:46:18 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.14 ms, Memory: 44.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,11 @@
+function solution(food) {
+    let left = '';
+    
+    for (let i = 1; i < food.length; i++) {
+        left += String(i).repeat(Math.floor(food[i] / 2));
+    }
+    
+    const right = left.split('').reverse().join('');
+    
+    return left + '0' + right;
+}
