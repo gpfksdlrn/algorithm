83b3366ab55c7daa2d43dfe013e4a31d6ca8ab41
hizdev <70252430+gpfksdlrn@users.noreply.github.com>
2026-07-02 10:13:49 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.65 ms, Memory: 49.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,10 @@
+function solution(t, p) {
+    let count = 0;
+    const len = p.length;
+    
+    for (let i = 0; i <= t.length - len; i++) {
+        if(t.slice(i, i + len) <= p) count++;
+    }
+    
+    return count;
+}
