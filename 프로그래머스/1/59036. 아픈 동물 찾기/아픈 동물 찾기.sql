0df100d02739e57cc9b6b692d7cf6961873b1729
hizdev <70252430+gpfksdlrn@users.noreply.github.com>
2026-07-03 15:16:12 +0900

    [level 1] Title: 아픈 동물 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -0,0 +1,12 @@
+-- ANIMAL_ID 아이디
+-- ANIMAL_TYPE 생물 종
+-- DATETIME 보호 시작일
+-- INTAKE_CONDITION 보호 시작 시 상태
+-- NAME 이름
+-- SEX_UPON_INTAKE 성별 및 중성화 여부
+
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME 
+FROM ANIMAL_INS 
+WHERE INTAKE_CONDITION = 'Sick'
+ORDER BY ANIMAL_ID ASC
