da8d3895cd8bb49c07e55b814b50e58a904368c3
hizdev <70252430+gpfksdlrn@users.noreply.github.com>
2026-08-03 17:47:00 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.09 ms, Memory: 44.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,15 @@
+// 두 수의 [최대공약수, 최소공배수] 반환
+function solution(n, m) {
+    const gcdValue = gcd(n, m);
+    const lcmValue = (n * m) / gcdValue;
+    
+    return [gcdValue, lcmValue];
+}
+
+// 최대공약수 구하는 함수 (유클리드 호제법)
+const gcd = (a, b) => {
+    while (b !== 0) {
+        [a, b] = [b, a % b];
+    }
+    return a;
+}
