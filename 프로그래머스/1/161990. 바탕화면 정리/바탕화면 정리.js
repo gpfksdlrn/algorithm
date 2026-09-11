db7aed52fc8cf97847ad2b44e87b75faface2861
hizdev <70252430+gpfksdlrn@users.noreply.github.com>
2026-09-11 18:07:02 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.32 ms, Memory: 43.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -0,0 +1,18 @@
+function solution(wallpaper) {
+    let lux = 50;
+    let luy = 50;
+    let rdx = 0;
+    let rdy = 0;
+    
+    for (let i = 0; i < wallpaper.length; i++) {
+        for (let j = 0; j < wallpaper[i].length; j++) {
+            if (wallpaper[i][j] === '#') {
+                lux = Math.min(lux, i);
+                luy = Math.min(luy, j);
+                rdx = Math.max(rdx, i);
+                rdy = Math.max(rdy, j);
+            }
+        }
+    }
+    return [lux, luy, rdx + 1, rdy + 1];
+}
