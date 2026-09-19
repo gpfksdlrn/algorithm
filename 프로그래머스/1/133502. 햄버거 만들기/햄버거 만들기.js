83175957f31ba83028521ab2bc966f49db04d463
hizdev <70252430+gpfksdlrn@users.noreply.github.com>
2026-09-19 22:30:31 +0900

    [level 1] Title: 햄버거 만들기, Time: 52.05 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,20 @@
+function solution(ingredient) {
+    const stack = [];
+    let count = 0;
+    
+    for (const item of ingredient) {
+        stack.push(item);
+        if (
+                stack.length >= 4 &&
+                stack[stack.length - 4] === 1 &&
+                stack[stack.length - 3] === 2 &&
+                stack[stack.length - 2] === 3 &&
+                stack[stack.length - 1] === 1
+        ) {
+            stack.length -= 4;
+            count++;
+        }
+    }
+    
+    return count;
+}
